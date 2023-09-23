5ee188d2e38398ea111b7745033f442f7e7a66b0
Nithin2701s <115217815+Nithin2701s@users.noreply.github.com>
2023-09-23 23:36:23 +0530

    Post method for added user to DB

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,13 @@ const connectMongo = require('./db')
 const express = require('express')
 const app = express()
 
+app.use(express.json())
 app.get('/',(req,res)=>{
     res.send("Hello World!")
 })
 app.use('/api/auth',require('./routes/auth'))
 // app.use('/api/notes',require('./routes/notes'))
-
+ 
 app.listen(4000,()=>{
     connectMongo()
     console.log('listening to port 4000')
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,13 @@
 const express =require('express')
 const router = express.Router();
+const user =require('../models/User');
+const User = require('../models/User');
 
-module.exports=router.get('/',(req,res)=>{
-   const obj={
-        a:'HI',
-        number:'27'
-    }
-    res.json(obj)
+// Create a user using post
+
+
+module.exports=router.post('/',(req,res)=>{
+ const User = user(req.body)
+ User.save()   
+    res.json("Added to DB")
 })
